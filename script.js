709c399962ccaf171282adb3fcbe83a0fb212537
aliceKjQin <aliceqininus@gmail.com>
2023-08-01 11:34:16 +0800

    second pass

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function createList() {
         listItem.setAttribute('data-index', index);
 
         listItem.innerHTML = `
-            <span class="number">${index + 1}<span>
+            <span class="number">${index + 1}</span>
             <div class="draggable" draggable="true">
                 <p class="person-name">${person}</p>
                 <i class="fas fa-grip-lines"></i>    
@@ -44,5 +44,79 @@ function createList() {
 
         draggable_list.appendChild(listItem);
     });
+
+    addEventListener();
 }
 
+function dragStart() {
+    // console.log('Event: ', 'dragstart')
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+    // console.log(dragStartIndex)
+}
+
+function dragOver(e) {
+    // console.log('Event: ', 'dragover')
+    e.preventDefault()
+}
+
+function dragDrop() {
+    // console.log('Event: ', 'drop')
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable')
+    const itemTwo = listItems[toIndex].querySelector('.draggable')
+
+    listItems[fromIndex].appendChild(itemTwo)
+    listItems[toIndex].appendChild(itemOne)
+ 
+}
+
+// Check the order of list items
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const personName = listItem.querySelector('.draggable').innerText.trim();
+
+        if(personName != richestPeople[index]) {
+            listItem.classList.add('wrong')
+        } else {
+            listItem.classList.remove('wrong')
+            listItem.classList.add('right')
+        }
+    })
+}
+
+
+
+function dragEnter() {
+    // console.log('Event: ', 'dragenter')
+    this.classList.add('over')
+}
+
+function dragLeave() {
+    // console.log('Event: ', 'dragleave')
+    this.classList.remove('over')
+}
+
+function addEventListener() {
+    const draggables = document.querySelectorAll('.draggable');
+    const draggableListItems = document.querySelectorAll('.draggable-list li');
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart);
+    })
+
+    draggableListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver);
+        item.addEventListener('drop', dragDrop);
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragleave', dragLeave);
+    })
+}
+
+check.addEventListener('click', checkOrder)
